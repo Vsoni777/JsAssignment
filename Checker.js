76c54e2e511f971ae479096333b344b3ddceb37c
Vsoni777 <viveksoni85963@gmail.com>
2024-12-06 18:05:05 +0530

    create javascript file for duplicate element using recursive way

---
+++ Checker.js
@@ -0,0 +1,41 @@
+
+function palid(text){
+    if(typeof text!='string'){
+        return "enter String";
+    }
+    if(text.length<1){
+        return true;
+    }
+    if(text[0]!==text[text.length-1]){
+        return false;
+    }
+    return text.slice(1,text.length-1);
+    
+}
+
+const text=document.getElementById('value');
+const button=document.getElementById('palid');
+ const message=document.getElementById('msg');
+button.addEventListener('click',()=>{
+    console.log(button.value);
+    const word=text.value.trim();
+    if(word===''){
+        message.textContent="Please enter valid String";
+        message.style.color='red'
+    }
+    if (!isNaN(word)) {
+        text.value='';
+         text.focus(); 
+        console.log("Integer value");
+    }
+    console.log(word);
+    const result=palid(word);
+
+    if(result){
+        message.textContent="palidrome";
+        message.style.color='green';
+    }else{
+        message.textContent="not palidrome";
+        message.style.color='blue';
+    }
+});
